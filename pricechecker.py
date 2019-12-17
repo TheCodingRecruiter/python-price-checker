cb899d284869cc8b6b7a5c2d1a2edc37b1af78bd
Richard Eby <richard.eby@hotmail.com>
2019-12-17 09:30:13 -0500

    Added a desired_rate variable to class object call
    Added a desired_rate variable to class object call.  This will allow the code to be more dynamic and use different desired prices for different items.

--- pricechecker.py
+++ pricechecker.py
@@ -11,8 +11,9 @@ class AmazonProduct:
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
     }
 
-    def __init__(self, URL):
+    def __init__(self, URL, desired_rate):
         self.URL = URL
+        self.desired_rate = desired_rate
         
 
 
@@ -40,6 +41,7 @@ class AmazonProduct:
 
 
     def run_price_checker(self):
+        desired_rate = self.desired_rate
         minute_length = 60
         day_length = 24
         price = self.check_price()
@@ -49,7 +51,7 @@ class AmazonProduct:
             runcount = runcount + 1
             print(f'{runcount} number of run throughs')
             price = self.check_price()
-            if price < 85:
+            if price < desired_rate:
                 loopover = False
                 print(f'Buy it Now. It dropped to ${price}')
                 print(URL)
@@ -57,9 +59,9 @@ class AmazonProduct:
                 print(f"Don't buy it yet.  It still costs ${price}")
                 time.sleep(minute_length * 10)
 
-blanket = AmazonProduct('https://www.amazon.com/ADVANCED-Dual-Control-Electric-Certified-Radiation/dp/B07WC58PQD/ref=sr_1_4?keywords=heated+blanket+queen&qid=1575126371&sr=8-4')
+blanket = AmazonProduct('https://www.amazon.com/ADVANCED-Dual-Control-Electric-Certified-Radiation/dp/B07WC58PQD/ref=sr_1_4?keywords=heated+blanket+queen&qid=1575126371&sr=8-4', 85)
 
-gloves = AmazonProduct('https://www.amazon.com/dp/B01M0SF4ZP/ref=sspa_dk_detail_0?psc=1&pd_rd_i=B01M0SF4ZP&pd_rd_w=EtM44&pf_rd_p=45a72588-80f7-4414-9851-786f6c16d42b&pd_rd_wg=aQCjF&pf_rd_r=YEXGNDMRQS9DT2B9GK58&pd_rd_r=1fe429c7-fe3b-430b-94e5-d9f60377f3d7&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyVkIxSjFJMkpER1Y2JmVuY3J5cHRlZElkPUEwMzE0NjEzRDNRMUFUMDFENVpSJmVuY3J5cHRlZEFkSWQ9QTEwMTAxNzAxS05EQVpZUDNPMUdRJndpZGdldE5hbWU9c3BfZGV0YWlsJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==')
+gloves = AmazonProduct('https://www.amazon.com/dp/B01M0SF4ZP/ref=sspa_dk_detail_0?psc=1&pd_rd_i=B01M0SF4ZP&pd_rd_w=EtM44&pf_rd_p=45a72588-80f7-4414-9851-786f6c16d42b&pd_rd_wg=aQCjF&pf_rd_r=YEXGNDMRQS9DT2B9GK58&pd_rd_r=1fe429c7-fe3b-430b-94e5-d9f60377f3d7&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyVkIxSjFJMkpER1Y2JmVuY3J5cHRlZElkPUEwMzE0NjEzRDNRMUFUMDFENVpSJmVuY3J5cHRlZEFkSWQ9QTEwMTAxNzAxS05EQVpZUDNPMUdRJndpZGdldE5hbWU9c3BfZGV0YWlsJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==', 65)
 
 gloves.run_price_checker()
 
